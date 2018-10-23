a04aff659bbc0d0ef678c3fe4850af08b3ee1af7
sabrinaM <sabrinamsh@gmail.com>
2018-10-23 20:14:35 +0200

    add login with fb

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import React, {
     Component
 } from 'react';
+import {
+    Button
+} from 'antd'
 import './App.css';
+import FacebookLogin from 'react-facebook-login';
+
 
 class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            user: null
+        }
+    }
+    responseFacebook = (response) => {
+        this.setState({
+            user: response
+        })
+    }
     render() {
         return ( <
-            div className = "App" > Hello < /div >
-        );
+            div className = "App" > {
+                this.state.user !== null ? `Hello, ${this.state.user.name}` : <
+                    FacebookLogin appId = "762214630794479"
+                autoLoad = {
+                    false
+                }
+                fields = "name,email,picture"
+                scope = "public_profile"
+                callback = {
+                    this.responseFacebook
+                }
+                />} < /
+                div >
+            );
+        }
     }
-}
 
-export default App;
+    export default App;
