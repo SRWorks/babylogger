684134616a649b45d15bfa3bc303af2f57d760ff
sabrinaM <sabrinamsh@gmail.com>
2018-10-23 13:44:58 +0200

    add antd as UI library

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, {
     Component
 } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from 'antd/lib/button';
+import 'antd/lib/button/style/css';
 
 class App extends Component {
     render() {
-        return (
-            <div className = "App" >
-            hello world
-            </div>
+        return ( <
+            div className = "App" >
+            hello world <
+            Button type = "primary" >
+            cat <
+            /Button> < /
+            div >
         );
     }
 }
