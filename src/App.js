f3cd4184e7ec9e90f9a47d857095f17a55ed704e
sabrinaM <sabrinamsh@gmail.com>
2018-10-23 16:42:44 +0200

    clean up

--- src/App.js
+++ src/App.js
@@ -2,18 +2,11 @@ import React, {
     Component
 } from 'react';
 import './App.css';
-import Button from 'antd/lib/button';
-import 'antd/lib/button/style/css';
 
 class App extends Component {
     render() {
         return ( <
-            div className = "App" >
-            hello world <
-            Button type = "primary" >
-            cat <
-            /Button> < /
-            div >
+            div className = "App" > Hello < /div >
         );
     }
 }
