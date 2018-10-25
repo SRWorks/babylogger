47901cd1179083fea006f2dc5e53dead5472af44
sabrinaM <sabrinamsh@gmail.com>
2018-10-25 10:02:46 +0200

    add mobx

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
-import React, {
-    Component
-} from 'react';
-import {
-    Button
-} from 'antd'
-import './App.css';
-import FacebookLogin from 'react-facebook-login';
+import React, { Component } from 'react'
+import './App.css'
+import FacebookLogin from 'react-facebook-login'
+import { extendObservable } from 'mobx'
+import { observer } from 'mobx-react'
 
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
+    constructor() {
+        super()
+
+        extendObservable(this, {
             user: null
-        }
-    }
-    responseFacebook = (response) => {
-        this.setState({
-            user: response
         })
+        // this.state = {user: null}
     }
+    responseFacebook = response => {
+        debugger
+        // this.setState({ user: response })
+        this.user = response
+    };
     render() {
-        return ( <
-            div className = "App" > {
-                this.state.user !== null ? `Hello, ${this.state.user.name}` : <
-                    FacebookLogin appId = "762214630794479"
-                autoLoad = {
-                    false
-                }
+        return (
+        <div className = "App">
+        { this.user !== null ? `Hello, ${this.user.name}`:
+            <FacebookLogin
+                appId = "762214630794479"
+                autoLoad = { false }
                 fields = "name,email,picture"
                 scope = "public_profile"
-                callback = {
-                    this.responseFacebook
-                }
-                />} < /
-                div >
-            );
+                callback = { this.responseFacebook }
+            />
         }
+        </div>
+        )
     }
+}
 
-    export default App;
+export default observer(App)
--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import moment from 'moment';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import moment from 'moment'
 
-moment.locale();
+moment.locale()
 
-ReactDOM.render( < App / > , document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root'))
+serviceWorker.unregister()
